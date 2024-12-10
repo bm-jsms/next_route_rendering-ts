6de281ee8e760b272446faf32134166c07545b70
Josué EmaxS <josueemaxsalazar04@gmail.com>
2024-12-10 04:15:28 -0500

    ✨ [add] Main header - component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import { MainHeader } from '@/components';
 
 export const metadata: Metadata = {
 	title: 'Create Next App',
@@ -13,7 +14,12 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='en'>
-			<body>{children}</body>
+			<body>
+				<div id='page'>
+					<MainHeader />
+					{children}
+				</div>
+			</body>
 		</html>
 	);
 }
