4022f88ecb82e11f163d0982962e190e68a5fa12
Josué EmaxS <josueemaxsalazar04@gmail.com>
2024-12-10 05:25:03 -0500

    🎉 [create] archive layout

---
+++ app/archive/layout.tsx
@@ -0,0 +1,16 @@
+type ArchiveLayoutProps = {
+	archive: React.ReactNode;
+	latest: React.ReactNode;
+};
+
+const ArchiveLayout = ({ archive, latest }: ArchiveLayoutProps) => {
+	return (
+		<div>
+			<h1>News archive</h1>
+			<section id='archive-filter'>{archive}</section>
+			<section id='archive-latest'>{latest}</section>
+		</div>
+	);
+};
+
+export default ArchiveLayout;
