9b4c50feffa484dd0311351487e62aea01280fb5
Josué EmaxS <josueemaxsalazar04@gmail.com>
2024-12-11 03:55:02 -0500

    ✨ [add] Link

--- app/news/[newsSlug]/page.tsx
+++ app/news/[newsSlug]/page.tsx
@@ -1,4 +1,5 @@
 import { DUMMY_NEWS } from '@/shared';
+import Link from 'next/link';
 import { notFound } from 'next/navigation';
 
 type NewsDetailPageProps = {
@@ -20,7 +21,9 @@ const NewsDetailPage = async ({ params }: NewsDetailPageProps) => {
 	return (
 		<article className='news-article'>
 			<header>
-				<img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+				<Link href={`/news/${newsItem.slug}/image`}>
+					<img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+				</Link>
 				<h1>{newsItem.title}</h1>
 				<time dateTime={newsItem.date}>{newsItem.date}</time>
 			</header>
